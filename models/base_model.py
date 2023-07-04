2a4ec83e0e34f4c0c343a2e4565dbbbdf08d1c89
fabianatellechea <fabianatellechea@gmail.com>
2023-07-04 14:49:55 -0700

    dict

--- models/base_model.py
+++ models/base_model.py
@@ -3,7 +3,6 @@
 import uuid
 from datetime import datetime
 
-
 class BaseModel:
     """ """
     def __init__(self):
@@ -20,3 +19,12 @@ class BaseModel:
     def save(self):
         """ """
         self.update_at = datetime.now()
+
+    def to_dict(self):
+        """ """
+
+        new_dict = dict(self.__dict__)
+        new_dict['__class__'] = self.__class__.__name__
+        new_dict['created_at'] = self.created_at.isoformat()
+        new_dict['updated_at'] = self.updated_at.isoformat()
+        return new_dict
